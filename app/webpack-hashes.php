f7f6977fb631a6328683d6f2eee261b4eb76159e
Wojciech Frącz <fraczwojciech@gmail.com>
2017-04-21 19:43:57 +0200

    Built 2.0

---
+++ app/webpack-hashes.php
@@ -0,0 +1,3 @@
+<?php
+/* Hash map generated automatically by running npm run build in src/Frontend directory. */
+return ["schedule-form.js"=>"schedule-form.c787489f26a51a9d2b45.js","iodevice-details.js"=>"iodevice-details.be03e9ee0da1f9439705.js","user-account.js"=>"user-account.8bb9a79f4ee1e308273d.js","schedule-list.js"=>"schedule-list.a3a6f09917618997fca5.js","commons.js"=>"commons.81ca938579e7a20490b5.js",];
